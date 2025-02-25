4f347e82e3868ca0b6a873ad5ad2b0432bca413f
jdav892 <jadavila9@gmail.com>
2025-02-25 02:49:07 -0500

    feat: declares struct to track backends, relays requests through reverse proxy, initializes server, and imports atomic counter to increment index

--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
   "fmt"
   "net/http"
+  "sync"
+  "sync/atomic"
 )
 
+//structs to hold backends
 type Backend struct {
   URL   *url.URL
   Alive   bool
   mux   sync.RWMutex
   ReverseProxy    *httputil.ReverseProxy
 }
+
+//struct to track all backends in balancer
+type ServerPool struct {
+  backends    []*Backend
+  current   uint64
+}
+
+//relays requests through ReverseProxy
+u, _ := url.Parse("http://localhost:8080")
+rp := httputil.NewSingleHostReverseProxy(u)
+
+
+//initializes server and adds handler
+http.HandlerFunc(rp.ServeHTTP)
+
+//increments index atomically 
+func (s *ServerPool) NextIndex() int {
+  return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+
 
 
