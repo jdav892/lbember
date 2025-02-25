dbb26c04e90060974ee1925c748c36963d4fee7e
jdav892 <jadavila9@gmail.com>
2025-02-25 03:03:01 -0500

    feat: picking up live backends by traversing the backends slice and marking the current backend

--- main.go
+++ main.go
@@ -34,6 +34,20 @@ func (s *ServerPool) NextIndex() int {
   return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
 }
 
-
-
-
+//GetNextPeer returns the next active peer to take a connection
+func (s *ServerPool) GetNextPeer() *Backend {
+  //loop entire backends to find out an Alive backend
+  next := s.NextIndex()
+  l := len(s.backends) + next //start from next and move a full cycle
+  for i := next; i < l; i++ {
+    idx := i % len(s.backends) //take an index by modding with length
+    //if backend is alive, use it and store if its not the original
+    if s.backends[idx].IsAlive() {
+      if != next {
+        atomic.StoreUint64(&s.current, uint64(idx)) //to mark current one
+      }
+      return s.backends[idx]
+    }
+  }
+  return nil
+}
